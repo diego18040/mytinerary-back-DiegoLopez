3aa5c9e4d7dd81b7ceed27fc45381cebd39d30ce
diego18040 <dieg00.lopez00@hotmail.com>
2024-11-05 20:39:29 -0600

    Correcion de fallos

--- mytinerary-Diego/src/components/Icon.jsx
+++
@@ -1,5 +0,0 @@
-const Icon = ({ icon, fn }) => {
-  return <img className="icon" onClick={() => fn()} src={icon} />;
-};
-
-export default Icon;
--- mytinerary-Diego/src/utils/apiURL.js
+++
@@ -1,2 +0,0 @@
-// export const apiURL = 'http://localhost:5000/api/';
-export const apiURL = import.meta.env.VITE_API_URL;
