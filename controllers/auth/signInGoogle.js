700684a8321731d2c15e33e8e121103c9b45cd98
diego18040 <dieg00.lopez00@hotmail.com>
2024-11-25 03:44:47 -0600

    Changes for google

--- controllers/auth/signInGoogle.js
+++ controllers/auth/signInGoogle.js
@@ -1,16 +1,30 @@
+import jwt from 'jsonwebtoken';
 import User from "../../models/User.js";
 
-export default async(req,res,next) => {
+export default async (req, res, next) => {
     try {
-        await User.findOneAndUpdate(
-            {email: req.body.email},
-            {online: true}
-        )
-        console.log(req);
-        
-       return res.redirect('https://google.com?email:' +req.user.email+'token'+req.token)
+        const user = await User.findOneAndUpdate(
+            { email: req.user.email }, // Asegúrate de que `req.user` tenga el email correcto.
+            { online: true },
+            { new: true }
+        );
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        // Generar un token
+        const token = jwt.sign(
+            { id: user._id, email: user.email },
+            process.env.SECRET,
+            { expiresIn: "1d" } // Tiempo de expiración del token
+        );
+
+        // Redirigir al frontend con el token
+        const redirectUrl = `http://localhost:5173/?token=${token}`;
+        return res.redirect(redirectUrl);
 
     } catch (error) {
-       next(error) 
+        next(error);
     }
-}
+};
--- models/User.js
+++ models/User.js
@@ -3,12 +3,13 @@ import { Schema, model } from "mongoose";
 let collection ='users3'
 let schema = new Schema({
     name:{type:String,required:true},
-    lastname:{type:String,required:true},
+    lastname:{type:String,required:false}, // This false by singin of google that why google is opcional lastname//
     email:{type:String,required:true},
     password:{type:String,required:true},
     photo:{type:String,required:true},
-    country:{type:String,required:true},
-    online:{type: Boolean}
+    country:{type:String,required:false},
+    online:{type: Boolean},
+    googleAccount: { type: Boolean, default: false },
 
 },{
     timestamps: true
